6ec4985dc5928bde360fa91cce2d03584523ec04
felicecesarano <cesarano.fc@gmail.com>
2024-11-07 14:30:43 +0100

    update

--- src/main/java/fantaParcoBack/controller/AuthController.java
+++ src/main/java/fantaParcoBack/controller/AuthController.java
@@ -4,6 +4,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import fantaParcoBack.entity.Login;
 import fantaParcoBack.entity.AuthResponse;
@@ -13,6 +14,7 @@ import java.util.Collections;
 import java.util.List;
 
 @RestController
+@RequestMapping("/api")
 public class AuthController {
 
     private final JwtTokenProvider jwtTokenProvider;
