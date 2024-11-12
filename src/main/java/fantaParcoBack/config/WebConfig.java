32a50e2b84e06b4e8fec6944f25616f5180072ae
felicecesarano <cesarano.fc@gmail.com>
2024-11-12 13:42:15 +0100

    UPDATE

--- src/main/java/fantaParcoBack/config/WebConfig.java
+++ src/main/java/fantaParcoBack/config/WebConfig.java
@@ -10,9 +10,10 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("https://fantaparcodeiprincipi-51ffd.web.app", "https://fantaback2.onrender.com")
+                .allowedOrigins("https://fantaparcodeiprincipi-51ffd.web.app", "https://fantaback2.onrender.com", "https://uptimerobot.com") // Aggiungi uptimerobot.com se è necessario
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
+
     }
 }
