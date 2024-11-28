7e1dc0396748856e6d92ff4cbbff2277d99c225a
Felice Cesarano <157111990+felicecesarano@users.noreply.github.com>
2024-11-28 20:57:28 +0100

    Update EmailService.java

--- src/main/java/fantaParcoBack/service/EmailService.java
+++ src/main/java/fantaParcoBack/service/EmailService.java
@@ -1,6 +1,6 @@
 package fantaParcoBack.service;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
@@ -15,6 +15,9 @@ public class EmailService {
     @Autowired
     private JavaMailSender emailSender;
 
+    @Value("${spring.mail.username}")
+    private String fromEmail;
+
     public void sendHtmlEmail(String to, String subject, String htmlContent, String logoPath) throws MessagingException {
         MimeMessage message = emailSender.createMimeMessage();
         MimeMessageHelper helper = new MimeMessageHelper(message, true);
@@ -24,7 +27,10 @@ public class EmailService {
         helper.setText(htmlContent, true);
 
         // Aggiungi il logo come allegato e usa un CID per referirlo nell'HTML
-        helper.addInline("logo", new ClassPathResource(logoPath)); // Assicurati che il logo sia nella cartella src/main/resources
+        helper.addInline("logo", new ClassPathResource(logoPath));
+
+        // Configura il mittente con nome personalizzato
+        helper.setFrom(fromEmail, "FantaParco Dei Principi");
 
         emailSender.send(message);
     }
