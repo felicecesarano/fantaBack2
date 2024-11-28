486b249c337940c4b694f496ba02e08db8d825fb
felicecesarano <cesarano.fc@gmail.com>
2024-11-28 21:03:20 +0100

    UPDATE

--- src/main/java/fantaParcoBack/service/EmailService.java
+++ src/main/java/fantaParcoBack/service/EmailService.java
@@ -1,6 +1,7 @@
 package fantaParcoBack.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
@@ -8,6 +9,7 @@ import org.springframework.stereotype.Service;
 
 import jakarta.mail.MessagingException;
 import jakarta.mail.internet.MimeMessage;
+import java.io.UnsupportedEncodingException;
 
 @Service
 public class EmailService {
@@ -15,17 +17,24 @@ public class EmailService {
     @Autowired
     private JavaMailSender emailSender;
 
-    public void sendHtmlEmail(String to, String subject, String htmlContent, String logoPath) throws MessagingException {
+    @Value("${spring.mail.username}")
+    private String fromEmail;
+
+    public void sendHtmlEmail(String to, String subject, String htmlContent, String logoPath) throws MessagingException, UnsupportedEncodingException {
         MimeMessage message = emailSender.createMimeMessage();
-        MimeMessageHelper helper = new MimeMessageHelper(message, true);
+        MimeMessageHelper helper = new MimeMessageHelper(message, true, "UTF-8"); // Specifica la codifica
 
         helper.setTo(to);
         helper.setSubject(subject);
         helper.setText(htmlContent, true);
 
         // Aggiungi il logo come allegato e usa un CID per referirlo nell'HTML
-        helper.addInline("logo", new ClassPathResource(logoPath)); // Assicurati che il logo sia nella cartella src/main/resources
+        helper.addInline("logo", new ClassPathResource(logoPath));
+
+        // Configura il mittente con nome personalizzato e codifica UTF-8
+        helper.setFrom(fromEmail, "FantaParco Dei Principi");
 
         emailSender.send(message);
     }
 }
+
